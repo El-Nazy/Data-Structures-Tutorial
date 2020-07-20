0a174293f37291ed01acb223e702f388d461a55f
El-Nazy <emmanuelchinazangene@gmail.com>
2020-07-20 12:48:15 -0500

    Added code to calculate the factorial of 1 to 1000

--- factorial.js
+++ factorial.js
@@ -1,7 +1,13 @@
 function factorial(n) {
-    let result = 1
+    let result = 1;
     for (let i = 1; i <= n; i++) {
         result *= i;
     }
     return result;
+}
+
+let start = 1, end = 1000;
+
+for (let i = start; i <= end; i++) {
+    console.log(factorial(i));
 }
