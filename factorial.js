4e9eac10c21d9ce74c2e788ff8cd930fddf5c8e1
El-Nazy <emmanuelchinazangene@gmail.com>
2020-07-21 12:18:46 -0500

    Updated factorial.js

--- factorial.js
+++ factorial.js
@@ -1,13 +1,11 @@
 function factorial(n) {
     let result = 1;
-    for (let i = 1; i <= n; i++) {
-        result *= i;
-    }
+    for (let i = 1; i <= n; i++) result *= i;
     return result;
 }
 
 let start = 1, end = 1000;
 
 for (let i = start; i <= end; i++) {
-    console.log(factorial(i));
+    console.log(i, "-", factorial(i));
 }
